d5231209ad4de3c1b105c751be1eb6bcc685a3b8
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-11-20 21:39:09 -0500

    Project Completed

--- js/index.js
+++ js/index.js
@@ -11,3 +11,15 @@ diceImages[0].setAttribute("src", randomImageSource1);
 diceImages[1].setAttribute("src", randomImageSource2);
 
 console.log("First Dice:", randomNumber1, "Second Dice:", randomNumber2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+document.getElementById("playAgainBtn").addEventListener("click", function () {
+  location.reload();
+});
